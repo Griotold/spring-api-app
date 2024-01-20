5a41a5cf2dc499f2a1606a59dbd2c44505a3f2d6
Griotold <gotjd9773@naver.com>
2024-01-20 17:13:54 +0900

    [feature/spring-cloud-openFeign] HealthCheckResponseDto 변경 - 버전차이 때문인지는 모르겠지만 기본생성자가 있어야 해서 수정

--- src/main/java/com/app/api/health/dto/HealthCheckResponseDto.java
+++ src/main/java/com/app/api/health/dto/HealthCheckResponseDto.java
@@ -1,12 +1,17 @@
 package com.app.api.health.dto;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import java.util.List;
 
-@Getter @Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class HealthCheckResponseDto {
     private String health;
     private List<String> activeProfiles;
+    @Builder
+    public HealthCheckResponseDto(String health, List<String> activeProfiles) {
+        this.health = health;
+        this.activeProfiles = activeProfiles;
+    }
 }
